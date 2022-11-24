3735ec50bdfe0e5fc31b82b230f5a3fb2deea43a
John Puddephatt <john@jdp.org.uk>
2022-11-24 13:35:18 +0000

    logos etc added

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <script setup>
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
 import InlineSvg from "vue-inline-svg";
-
-import Logo from "./components/Logo.vue";
-
-const selectedStrand = ref(null);
-const playing = ref(false);
 
 const strands = [
   {
     angle: 3.92699,
     color: "#FFD93C",
-    name: "Hyde & Seek",
-    logo: "logos/bad-film-theory.svg",
-    description:
-      "For people crazy about documentaries that explore the many wonders of the world",
+    name: "Hyde Park Picture House",
+    logo: "logos/HPPH.svg",
   },
   {
     angle: 3.5342917,
     color: "#B389FF",
+    video:
+      "https://res.cloudinary.com/hpph/video/upload/t_lowquality/membership/f5aa960b-eb19-41b5-a48f-c1c36e4472d3.mp4",
     name: "Tuesday Wonders",
+    logo: "logos/TuesdayWonders.svg",
     description:
       "For people crazy about documentaries that explore the many wonders of the world",
   },
@@ -27,27 +23,49 @@ const strands = [
     angle: 3.14159,
     color: "#FD4AC2",
     name: "Creatures of the night",
+    logo: "logos/CreatureoftheNight.svg",
     description:
       "For people crazy about documentaries that explore the many wonders of the world",
   },
   {
     angle: 2.7488936,
     color: "#A0F0CB",
-    name: "Hyde &amp; Seek",
+    name: "Re-run",
+    logo: "logos/ReRun.svg",
     description:
       "For people crazy about documentaries that explore the many wonders of the world",
   },
   {
     angle: 2.35619,
     color: "#E5E3BF",
-    name: "Hyde &amp; Seek",
+    name: "Philosophy &amp; Film",
+    logo: "logos/PhilosophyFilm.svg",
+    description:
+      "?For people crazy about documentaries that explore the many wonders of the world",
+  },
+
+  {
+    angle: 0.785398,
+    color: "#f2acd7",
+    name: "Bad Film Theory",
+    logo: "logos/bad-film-theory.svg",
+    description:
+      "For people crazy about documentaries that explore the many wonders of the world",
+  },
+
+  {
+    angle: 0.3926991,
+    color: "#FE9BA4",
+    name: "Cinema Africa!",
+    logo: "logos/CinemaAfrica.svg",
     description:
       "For people crazy about documentaries that explore the many wonders of the world",
   },
   {
-    angle: -0.785398,
-    color: "#FEAD52",
-    name: "Hyde & Seek",
+    angle: 0,
+    color: "#45CDFF",
+    name: "Bring Your Own Baby",
+    logo: "logos/BYOB.svg",
     description:
       "For people crazy about documentaries that explore the many wonders of the world",
   },
@@ -55,27 +73,15 @@ const strands = [
     angle: -0.3926991,
     color: "#6EFF69",
     name: "Memory Matinees",
+    logo: "logos/MemoryMatinees.svg",
     description:
       "For people crazy about documentaries that explore the many wonders of the world",
   },
   {
-    angle: 0,
-    color: "#45CDFF",
-    name: "Creatures of the night",
-    description:
-      "For people crazy about documentaries that explore the many wonders of the world",
-  },
-  {
-    angle: 0.3926991,
-    color: "#FE9BA4",
-    name: "Hyde &amp; Seek",
-    description:
-      "For people crazy about documentaries that explore the many wonders of the world",
-  },
-  {
-    angle: 0.785398,
-    color: "#FF3E50",
-    name: "Hyde &amp; Seek",
+    angle: -0.785398,
+    color: "#FEAD52",
+    name: "Hyde & Seek",
+    logo: "logos/Hyde&Seek.svg",
     description:
       "For people crazy about documentaries that explore the many wonders of the world",
   },
@@ -83,79 +89,131 @@ const strands = [
 
 const faqs = [
   {
+    summary: "Is HPPH Membership different to the Friends of Hyde Park?",
+    details: "These are the details here like this.",
+  },
+  {
     summary: "How will becoming a member support my community?",
     details: "These are the details here like this.",
   },
   {
-    summary: "When will my membership start?",
+    summary: "When will my membership begin and expire?",
     details: "These are the details here like this.",
   },
   {
-    summary: "What is Pay As You Feel?",
+    summary: "Why are there no more membership cards?",
     details: "These are the details here like this.",
   },
   {
-    summary: "Another question?",
+    summary: "Why are you no longer posting programmes?",
+    details: "These are the details here like this.",
+  },
+  {
+    summary: "What is auto-renew?",
     details: "These are the details here like this.",
   },
 ];
 
 const benefits = [
-  "Priority booking on special events",
+  "HPPH Membership",
   "Free tickets",
-  "Become a member",
-  "Food/drink discounts",
-  "3 months free MUBI",
+
+  "HPPH Membership",
+  "Food/drink discount",
+
+  "HPPH Membership",
+  "Priority booking",
+
+  "HPPH Membership",
+  "Members’ newsletter",
+
+  "HPPH Membership",
+  "Ticket discount",
+
+  "HPPH Membership",
   "Limited-edition pin-badge",
+
+  "HPPH Membership",
+  "Three months free MUBI",
 ];
 
 const memberships = [
   {
     explainer:
-      "Choose this if you‘re not able to visit the cinema regularly but would like to support the cinema.",
+      "Are you unable to visit regularly but keen to support the work we do? This membership is for you.",
     image: "/images/16mm.png",
-    logo: "",
+    logo: "/logos/16MM.svg",
     price: "16",
     benefits: [
-      "HPPH member newsletter",
+      "Members’ newsletter (inc. exclusive offers)",
       "Priority booking on special events",
-      "3 months free MUBI",
+      "Three months free MUBI",
+      "Limited edition pin badge",
     ],
-    url: "https://hydeparkpicturehouse.co.uk/membership/16mm",
+    signup_link: "https://hydeparkpicturehouse.co.uk/membership/16mm",
+    gift_link: "https://hydeparkpicturehouse.co.uk/membership/16mm",
   },
   {
     explainer:
-      "Choose this if you plan to visit the cinema at least once every 3 months and regularly attend by yourself.",
+      "Do you expect to buy four or more tickets a year? This membership will save you money.",
     image: "/images/35mm.png",
-    logo: "",
+    logo: "/logos/35MM.svg",
     price: "35",
     benefits: [
-      "HPPH member newsletter",
+      "Members’ newsletter (inc. exclusive offers)",
       "Priority booking on special events",
-      "3 months free MUBI",
+      "Three months free MUBI",
+      "Limited edition pin badge",
       "Four free tickets (worth £36 full price)",
-      "Then £1 off tickets",
-      "15% off selected food/drink",
-      "Limited edition silver pin badge",
+      "Then £1 off tickets (once per film)",
+      "10% off selected food/drink",
     ],
-    url: "https://hydeparkpicturehouse.co.uk/membership/35mm",
+    gift_link: "https://hydeparkpicturehouse.co.uk/membership/35mm",
+    signup_link: "https://hydeparkpicturehouse.co.uk/membership/35mm",
   },
   {
-    explainer: "Choose this if you‘r...",
+    explainer:
+      "Do you expect to buy eight or more tickets a year? This membership will save you even more money.",
     image: "/images/70mm.png",
-    logo: "",
+    logo: "/logos/70MM.svg",
     price: "70",
     benefits: [
-      "HPPH member newsletter",
+      "Members’ newsletter (inc. exclusive offers)",
       "Priority booking on special events",
-      "3 months free MUBI",
+      "Three months free MUBI",
+      "Limited edition pin badge",
       "Eight free tickets (worth £72 full price)",
-      "Then £1.50 off tickets",
-      "25% off selected food/drink",
-      "Limited edition gold pin badge",
+      "Then £1.50 off tickets (up to twice per film)",
+      "15% off selected food/drink",
     ],
+    gift_link: "https://hydeparkpicturehouse.co.uk/membership/70mm",
+    signup_link: "https://hydeparkpicturehouse.co.uk/membership/70mm",
   },
 ];
+
+const selectedStrandIndex = ref(0);
+
+const selectedStrand = computed(() => {
+  return strands[selectedStrandIndex.value];
+});
+
+const playing = ref(false);
+
+function increaseStrandIndex() {
+  if (selectedStrandIndex.value < strands.length - 1) {
+    selectedStrandIndex.value++;
+  } else {
+    selectedStrandIndex.value = 0;
+  }
+}
+
+function decreaseStrandIndex() {
+  if (selectedStrandIndex.value > 0) {
+    selectedStrandIndex.value--;
+  } else {
+    selectedStrandIndex.value = strands.length - 1;
+  }
+}
 </script>
 
 <template>
@@ -172,7 +230,7 @@ const memberships = [
     </h3>
 
     <video
-      v-if="selectedStrand"
+      v-if="selectedStrand.video"
       class="inset-0 absolute transition duration-1000 w-full h-full object-cover object-center"
       :class="{ 'opacity-50': playing, 'opacity-0': !playing }"
       @playing="playing = true"
@@ -182,23 +240,57 @@ const memberships = [
       muted=""
       playsinline=""
     >
-      <source
-        type="video/mp4"
-        src="https://res.cloudinary.com/hpph/video/upload/t_lowquality/membership/f5aa960b-eb19-41b5-a48f-c1c36e4472d3.mp4"
-      />
-      <source
-        type="video/webm"
-        src="https://res.cloudinary.com/hpph/video/upload/t_webm/membership/f5aa960b-eb19-41b5-a48f-c1c36e4472d3.mp4"
-      />
+      <source type="video/mp4" :src="selectedStrand.video" />
     </video>
 
     <div
-      class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      class="absolute z-10 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
     >
-      <Logo v-if="!selectedStrand" class="w-64 text-yellow" />
+      <button
+        class="w-10 h-10 right-full lg:right-[50vh] top-1/2 -mt-4 absolute z-40"
+        @click="increaseStrandIndex"
+        aria-label="Go to previous strand"
+      >
+        <svg
+          class="transform rotate-180 origin-center w-full"
+          xmlns="http://www.w3.org/2000/svg"
+          width="17.575"
+          height="31.788"
+          viewBox="0 0 17.575 31.788"
+        >
+          <path
+            d="M0,0,14.744,14.723,0,29.447"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-width="1"
+          />
+        </svg>
+      </button>
+      <button
+        class="w-10 h-10 left-full lg:left-[50vh] top-1/2 absolute -mt-4 rounded-full z-40"
+        @click="decreaseStrandIndex"
+        aria-label="Go to next strand"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="17.575"
+          height="31.788"
+          viewBox="0 0 17.575 31.788"
+          class="w-full"
+        >
+          <path
+            d="M0,0,14.744,14.723,0,29.447"
+            fill="none"
+            stroke="#fff"
+            stroke-linecap="round"
+            stroke-width="1"
+          />
+        </svg>
+      </button>
+
       <div
-        v-else
-        class="type-label max-w-xs text-center"
+        class="type-label max-w-xs text-center relative"
         :style="`color: ${selectedStrand.color}`"
       >
         <inline-svg
@@ -208,28 +300,30 @@ const memberships = [
           @error=""
           width="150"
           height="150"
-          class="mx-auto w-64 mb-8"
+          class="mx-auto w-64"
         ></inline-svg>
-        {{ selectedStrand.description }}
+        <div class="absolute top-full pt-12 leading-normal">
+          {{ selectedStrand.description }}
+        </div>
       </div>
     </div>
 
     <div
-      class="absolute top-0 left-1/2 transform -translate-x-1/2 h-full aspect-square"
+      class="hidden lg:block absolute top-0 left-1/2 transform -translate-x-1/2 h-full aspect-square"
     >
       <button
         v-for="(strand, key) in strands"
-        @click="selectedStrand = strand"
+        @click="selectedStrandIndex = key"
         class="w-4 h-4 rounded-full left-1/2 top-1/2 absolute hover:scale-125 transition transform"
         :style="`background-color: ${strand.color};--tw-translate-x : ${
           Math.cos(strand.angle) * 50
         }vh; --tw-translate-y: ${Math.sin(strand.angle) * 50}vh;`"
-        :class="[strand.name == selectedStrand?.name ? '!scale-150' : '']"
+        :class="[selectedStrandIndex == key ? '!scale-150' : '']"
       ></button>
     </div>
 
     <a
-      class="absolute rounded-full block left-1/2 transform -translate-x-1/2 bottom-6 p-3 w-12 h-12 bg-black mx-auto bg-opacity-10"
+      class="absolute rounded-full block left-1/2 transform -translate-x-1/2 bottom-6 p-3 w-12 h-12 bg-gray-dark mx-auto bg-opacity-30 hover:bg-opacity-60"
       href="#memberships"
       aria-label="View memberships"
     >
@@ -252,24 +346,30 @@ const memberships = [
     class="bg-yellow"
     :style="`background-color: ${selectedStrand?.color}`"
   >
-    <div class="container max-w-5xl text-center mx-auto">
+    <div class="container max-w-5xl text-center mx-auto px-4">
       <p class="type-label pt-6 pb-12">Memberships</p>
-      <h2 class="type-h3 pb-24">
-        HPPH members enjoy lots of great benefits and help everyone in our
-        community experience the magic of cinema.
+      <h2 class="type-h5 lg:type-h3 pb-24 max-w-sm lg:max-w-4xl mx-auto">
+        As we prepare to reopen this spring, we’re excited to launch our new
+        HPPH Memberships.
       </h2>
     </div>
   </div>
   <div class="bg-gray">
-    <div class="container max-w-2xl text-center mx-auto py-16 space-y-6">
+    <div class="container max-w-3xl text-center mx-auto px-4 py-16 space-y-6">
       <p>
-        By becoming a member you’ll have access to some brilliant benefits –
-        including free and discounted tickets, money off food and drink and
-        priority booking for special events.
+        HPPH Members will not only enjoy loads of great benefits, but they’ll
+        also help us deliver even more accessible screenings – so everyone in
+        our communtiy can experience the magic of cinema.
       </p>
       <p>
-        You’ll also be helping us deliver more accessible screenings for our
-        community, giving everyone the opportunity to visit the cinema.
+        Lasting 12 months from the day the cinema reopens, HPPH Memberships will
+        help audiences save money thanks to free and discounted tickets, money
+        off food and drink and other exclusive offers.
+      </p>
+      <p>
+        With limited-edition pin badges given out to the first 500 members –
+        whether it’s for yourself or as a gift for a loved one this Christmas –
+        now’s the perfect time to become a HPPH Member.
       </p>
     </div>
   </div>
@@ -281,17 +381,19 @@ const memberships = [
     <div class="mt-24 grid lg:grid-cols-3 gap-4">
       <div v-for="membership in memberships" class="flex flex-col">
         <div class="relative">
-          <img :src="membership.image" />
-          <div
-            class="bg-yellow rounded-full w-32 h-16 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          ></div>
+          <img class="aspect-[4/3]" :src="membership.image" />
+
+          <img
+            :src="membership.logo"
+            class="w-32 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+
           <div class="right-0 absolute bottom-0 left-0">
-            <div class="type-label max-w-xs mx-auto mb-4">
+            <div class="type-label max-w-xs mx-auto mb-4 !leading-normal">
               {{ membership.explainer }}
             </div>
           </div>
         </div>
-        <img :src="membership.logo" />
 
         <div class="bg-yellow text-center py-6 text-black">
           <span class="type-h5">£{{ membership.price }}</span
@@ -306,22 +408,41 @@ const memberships = [
             {{ benefit }}
           </li>
         </ul>
-        <div class="type-subtitle bg-yellow text-center py-6 text-black">
-          Add to basket
-        </div>
+        <a
+          :href="membership.signup_link"
+          class="type-subtitle bg-yellow text-center py-6 text-black"
+        >
+          Sign-up here
+        </a>
+        <a
+          :href="membership.gift_link"
+          class="type-subtitle bg-gray-dark text-center py-6 text-white"
+        >
+          Purchase as a gift
+        </a>
       </div>
     </div>
   </div>
 
-  <div class="bg-black py-36 text-center">
-    <h2 class="type-h4 text-white mb-4">Are you aged 16–25?</h2>
+  <div class="bg-black py-36 text-center px-4 relative overflow-x-hidden">
+    <img
+      src="/images/1625-placeholder.jpg"
+      alt=""
+      class="mt-4 absolute top-1/2 left-1/2 transform -translate-y-1/2"
+    />
+    <h2 class="type-h5 lg:type-h4 text-white mb-4">Are you aged 15–25?</h2>
     <p class="type-label mb-24 text-white">
       Join our free membership for £5 tickets
     </p>
 
     <div
-      class="border border-gray-medium rounded-lg p-12 w-full max-w-lg aspect-video mx-auto relative"
+      class="border border-gray-medium rounded-lg p-12 w-full max-w-lg aspect-[441/278] mx-auto relative"
     >
+      <inline-svg
+        src="/logos/HPPH-1525.svg"
+        class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mx-auto w-40"
+      ></inline-svg>
+
       <div
         class="type-label absolute left-0 transform text-gray-medium top-1/2 -rotate-90 origin-center"
       >
@@ -331,8 +452,8 @@ const memberships = [
 
     <a
       target="_blank"
-      aria-label="Add 16-25 membership to basket"
-      class="rounded-full w-12 p-3 border mx-auto mt-12 border-gray-dark block"
+      aria-label="Purchase 15-25 membership"
+      class="rounded-full w-12 p-3 border mx-auto mt-12 border-gray-dark hover:bg-white block hover:bg-opacity-25"
       href="https://hydeparkpicturehouse.co.uk/membership/16-25/"
     >
       <svg
@@ -348,18 +469,22 @@ const memberships = [
         </g>
       </svg>
     </a>
-    <div class="type-subtitle text-center mt-6 text-yellow">Add to basket</div>
+    <a
+      href="https://hydeparkpicturehouse.co.uk/membership/16-25/"
+      class="type-subtitle inline-block text-center mt-4 text-yellow"
+      >Sign up here</a
+    >
   </div>
 
-  <div class="bg-yellow py-6 text-lg text-black whitespace-nowrap">
+  <div
+    class="bg-yellow py-6 text-lg text-black whitespace-nowrap w-screen overflow-hidden"
+  >
     <div class="flex flex-row w-[500vw]">
       <div class="marquee">
         <span
           class="inline-block"
           v-for="(benefit, key) in benefits.concat(benefits).concat(benefits)"
-          ><span :class="{ 'font-bold': !(key % benefits.length) }">{{
-            benefit
-          }}</span
+          ><span :class="{ 'font-bold': !(key % 2) }">{{ benefit }}</span
           >&nbsp;&bullet;&nbsp;
         </span>
       </div>
@@ -371,7 +496,9 @@ const memberships = [
       <h2 class="type-subtitle text-yellow">Information &amp; FAQs</h2>
       <div class="divide-y divide-gray-dark">
         <details v-for="faq in faqs">
-          <summary class="type-subtitle py-6 flex justify-between gap-2">
+          <summary
+            class="lg:type-subtitle leading-tight font-bold py-6 flex justify-between gap-2"
+          >
             {{ faq.summary }}
             <svg
               xmlns="http://www.w3.org/2000/svg"
